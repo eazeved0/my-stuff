05b48aeeddde165f8ef080431f194f9bd6890bc7
eazeved0 <eazevedo@bsd.com.br>
2023-01-11 13:42:45 -0300

    Update migrate_databases.sh

--- migrate_databases.sh
+++ migrate_databases.sh
@@ -3,7 +3,7 @@ cd /home/ssm-user
 echo "Installing dependencies..."
 curl -s https://raw.githubusercontent.com/eazeved0/my-stuff/main/pgdg.repo -O
 sudo mv pgdg.repo /etc/yum.repos.d/
-sudo amazon-linux-extras install epel -y 2>&1 /dev/null
+sudo amazon-linux-extras install epel -y 2>&1 1>/dev/null
 sudo yum install postgresql14 -y -q
 curl -s https://raw.githubusercontent.com/eazeved0/my-stuff/main/.pgpass -O
 sudo chmod 0600 /home/ssm-user/.pgpass
