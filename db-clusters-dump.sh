1954d4293e3e5afd440c9e5b878b916936c00c85
eazeved0 <eazevedo@bsd.com.br>
2023-01-11 18:54:47 -0300

    Update db-clusters-dump.sh

--- db-clusters-dump.sh
+++ db-clusters-dump.sh
@@ -13,6 +13,7 @@ export PGPASSFILE=~/.pgpass
 date=$(date +%d-%m-%y)
 echo "#!/bin/bash" |tee -a dump_all.sh 2>&1 1>/dev/null
 echo "Creating Datasets"
+
 clusters=$(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[] | .DBClusterIdentifier' |tr -d \" |grep -v destaxa-dev-commons)
 for i in $clusters ; do
 	endpoint=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .ReaderEndpoint' |tr -d \")
@@ -22,6 +23,8 @@ for i in $clusters ; do
 	echo "PGPASSFILE=~/.pgpass pg_dump -Fc -h $endpoint -p 5432 -U $username $database | aws s3 cp - s3://rds-backups-automation/pg_dump/backup_$i-$date.dump" |tee -a dump_all.sh 2>&1 1>/dev/null
 	chmod +x dump_all.sh
 done
-aws s3 --region sa-east-1  cp dump_all.sh  s3://rds-backups-automation/pg_dump/dump_all.sh
-echo "Dumping dbs to S3.."
+sed -i.bu 's/null/postgres/' dump_all.sh
+aws s3 --region sa-east-1  cp dump_all.sh  s3://rds-backups-automation/pg_dump/dump_all.sh 2>&1 1>/dev/null
+echo "Dumping dbs to S3..."
 chmod +x dump_all.sh && ./dump_all.sh
+echo "All done!"
