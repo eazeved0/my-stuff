45a75f71bfc814bef1cb2e61cd8d4120b62b7d40
eazeved0 <eazevedo@bsd.com.br>
2023-01-11 18:42:42 -0300

    Update db-clusters-dump.sh

--- db-clusters-dump.sh
+++ db-clusters-dump.sh
@@ -15,13 +15,13 @@ echo "#!/bin/bash" |tee -a dump_all.sh 2>&1 1>/dev/null
 echo "Creating Datasets"
 
 for i in $clusters ; do
-	endpoint=$(aws rds --region sa-east-1 --profile destaxa describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .ReaderEndpoint' |tr -d \")
-	username=$(aws rds --region sa-east-1 --profile destaxa describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
-	database=$(aws rds --region sa-east-1 --profile destaxa describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
+	endpoint=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .ReaderEndpoint' |tr -d \")
+	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
+	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "echo \"Creating $database Backup\" " |tee -a dump_all.sh 2>&1 1>/dev/null
 	echo "PGPASSFILE=~/.pgpass pg_dump -Fc -h $endpoint -p 5432 -U $username $database | aws s3 cp - s3://rds-backups-automation/pg_dump/backup_$i-$date.dump" |tee -a dump_all.sh 2>&1 1>/dev/null
 	chmod +x dump_all.sh
 done
-aws s3 --region sa-east-1 --profile destaxa cp dump_all.sh  s3://rds-backups-automation/pg_dump/dump_all.sh
+aws s3 --region sa-east-1  cp dump_all.sh  s3://rds-backups-automation/pg_dump/dump_all.sh
 echo "Dumping dbs to S3.."
 chmod +x dump_all.sh && ./dump_all.sh
