729d87f9b68d47cdc5a6d5eb0d83f069ead22246
eazeved0 <eazevedo@bsd.com.br>
2023-01-13 11:56:56 -0300

    Update db-clusters-dump.sh

--- db-clusters-dump.sh
+++ db-clusters-dump.sh
@@ -20,7 +20,6 @@ for i in $clusters ; do
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "echo \"Creating $database Backup\" " |tee -a dump_all.sh 2>&1 1>/dev/null
-	pg_dump --create -s -U postgres -d test1
 	echo "PGPASSFILE=~/.pgpass pg_dump --create -s -h $i -U $username -d $database | aws s3 cp - s3://rds-backups-automation/pg_dump/backup_$i-$date.sql" |tee -a dump_all.sh 2>&1 1>/dev/null
 	chmod +x dump_all.sh
 done
