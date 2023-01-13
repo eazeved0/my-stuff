5a308d0f0ef1bede3f945c5492d3f93bade3faed
eazeved0 <eazevedo@bsd.com.br>
2023-01-13 12:00:46 -0300

    Update db-clusters-restore.sh

--- db-clusters-restore.sh
+++ db-clusters-restore.sh
@@ -19,20 +19,20 @@ mkdir -p db_dumps/auth && mkdir db_dumps/backoffice
 
 backoffice=$(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[] | .DBClusterIdentifier' |tr -d \" |grep -Ev 'kong|keycloak')
 for i in $backoffice ; do
-  aws s3 cp s3://rds-backups-automation/pg_dump/backup_$i-$date.dump db_dumps/backoffice/
+  aws s3 cp s3://rds-backups-automation/pg_dump/backup_$i-$date.sql db_dumps/backoffice/
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "Restoring DB $database into Aurora BackOffice Cluster"
-	PGPASSFILE=~/.pgpass pg_restore -h $i -U $username -C -d postgres < db_dumps/backoffice/backup_$i-$date.dump"
+	PGPASSFILE=~/.pgpass pg_restore -Ft -h $i -U $username -C -d $database < db_dumps/backoffice/backup_$i-$date.sql"
   echo "Done restoring DBs to Aurora BackOffice Cluster"
 done
 
 for auth in $(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[] | .DBClusterIdentifier' |tr -d \" |grep -E 'kong|keycloak') ; do
-  aws s3 cp s3://rds-backups-automation/pg_dump/backup_$i-$date.dump db_dumps/auth/
+  aws s3 cp s3://rds-backups-automation/pg_dump/backup_$i-$date.sql db_dumps/auth/
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "Restoring DB $auth into Aurora Auth Cluster"
-	PGPASSFILE=~/.pgpass pg_restore -h $auth -U $username -C -d postgres < < db_dumps/auth/backup_$i-$date.dump"
+	PGPASSFILE=~/.pgpass pg_restore -Ft -h $i -U $username -C -d $database < db_dumps/auth/backup_$i-$date.sql"
   echo "Done restoring DBs to Aurora Auth Cluster"
 done  
 
