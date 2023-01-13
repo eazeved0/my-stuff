9655e1fa801e1d56500079cf8fbedacee8b8db4c
eazeved0 <eazevedo@bsd.com.br>
2023-01-13 13:47:03 -0300

    Update db-clusters-restore.sh

--- db-clusters-restore.sh
+++ db-clusters-restore.sh
@@ -23,7 +23,7 @@ for i in $backoffice ; do
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "Restoring DB $database into Aurora BackOffice Cluster"
-	PGPASSFILE=~/.pgpass pg_restore --create -Ft -h $backofficerds -U $username -C -d postgres  < db_dumps/backoffice/backup_$i-$date.sql
+	PGPASSFILE=~/.pgpass pg_restore -c --create -Ft -h $backofficerds -U $username -C -d postgres  < db_dumps/backoffice/backup_$i-$date.sql
   echo "Done restoring DBs to Aurora BackOffice Cluster"
 done
 
@@ -32,7 +32,7 @@ for auth in $(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "Restoring DB $auth into Aurora Auth Cluster"
-	PGPASSFILE=~/.pgpass pg_restore --create -Ft -h $authrds -U $username -C -d postgres < db_dumps/auth/backup_$auth-$date.sql
+	PGPASSFILE=~/.pgpass pg_restore -c --create -Ft -h $authrds -U $username -C -d postgres < db_dumps/auth/backup_$auth-$date.sql
   echo "Done restoring DBs to Aurora Auth Cluster"
 done  
 
