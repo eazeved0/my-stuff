e40fb0290596fb617571e40fa0f1b05dc2a2bdcc
eazeved0 <eazevedo@bsd.com.br>
2023-02-08 16:06:30 -0300

    Update db-clusters-dump.sh

--- db-clusters-dump.sh
+++ db-clusters-dump.sh
@@ -15,13 +15,13 @@ date=$(date +%d-%m-%y)
 echo "#!/bin/bash" |tee dump_all.sh 2>&1 1>/dev/null
 echo "Preparing Manifests"
 
-clusters=$(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[] | .DBClusterIdentifier' |tr -d \" |grep -Ev 'destaxa-dev-commons|destaxa-dev-auth|destaxa-dev-backoffice|destaxa-bpm')
+clusters=$(aws rds --region sa-east-1 describe-db-clusters | jq '.DBClusters[] | .DBClusterIdentifier' |tr -d \" |grep -E 'kong|keycloak')
 for i in $clusters ; do
 	endpoint=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .ReaderEndpoint' |tr -d \")
 	username=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .MasterUsername' |tr -d \")
 	database=$(aws rds --region sa-east-1  describe-db-clusters --db-cluster-identifier $i |jq '.DBClusters[] .DatabaseName' |tr -d \")
 	echo "echo \"Creating $database Backup\" " |tee -a dump_all.sh 2>&1 1>/dev/null
-	echo "PGPASSFILE=~/.pgpass pg_dump -Ft --create -s -h $endpoint -U $username -d $database | aws s3 cp - s3://rds-backups-automation/pg_dump/backup_$i-$date.sql" |tee -a dump_all.sh 2>&1 1>/dev/null
+	echo "PGPASSFILE=~/.pgpass pg_dump -h $endpoint -U $username $database | aws s3 cp - s3://rds-backups-automation/pg_dump/backup_$i-$date.sql" |tee -a dump_all.sh 2>&1 1>/dev/null
 done
 sed -i.bu 's/null/postgres/' dump_all.sh
 aws s3 --region sa-east-1  cp dump_all.sh  s3://rds-backups-automation/pg_dump/dump_all.sh 2>&1 1>/dev/null
